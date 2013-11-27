8b960988e3952aaf3e6bde0b1b2e24d1925d7fce
bambam922 <bambam922@890333c5-fed3-4d1d-b8f7-946a52a8b8fd>
2013-11-27 09:48:13 +0000

    ...

--- MrItemRemover2DEV/MrItemRemover2/MrItemRemover2GUI.cs
+++ MrItemRemover2DEV/MrItemRemover2/MrItemRemover2GUI.cs
@@ -153,7 +153,7 @@ namespace MrItemRemover2
 
         public void SettingsDebug()
         {
-            Dlog("Enable Selling   = {0}", EnableSell.Checked);
+            //Dlog("Enable Selling   = {0}", EnableSell.Checked);
             Dlog("Remove Grays     = {0}", GrayItems.Checked);
             Dlog("Remove Whites    = {0}", WhiteItems.Checked);
             Dlog("Remove Greens    = {0}", GreenItems.Checked);
@@ -163,7 +163,7 @@ namespace MrItemRemover2
             Dlog("Sell Greens      = {0}", SellGreen.Checked);
             Dlog("Sell Blues       = {0}", SellBlue.Checked);
             Dlog("Sell Soulbound   = {0}", SellSoulbound.Checked);
-            Dlog("Check After Loot = {0}", LootEnable.Checked);
+           // Dlog("Check After Loot = {0}", LootEnable.Checked);
             Dlog("Gold Value       = {0}", GoldGrays.Text);
             Dlog("Silver Value     = {0}", SilverGrays.Text);
             Dlog("Copper Value     = {0}", CopperGrays.Text);
